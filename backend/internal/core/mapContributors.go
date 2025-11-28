68441ae7b68c37c40d08870a4c194de551c2303c
xLightly <26022006ik@gmail.com>
2025-11-29 01:01:47 +0300

    change map entity fields and add entity constructors

--- backend/internal/core/mapContributors.go
+++ backend/internal/core/mapContributors.go
@@ -1,9 +1,22 @@
 package core
 
-type role string
+import "errors"
+
+type contributorRole string
 
 type MapContributors struct {
-	MapId  string `db:"map_id"`
-	UserId string `db:"user_id"`
-	Role   role   `db:"role"`
+	MapId  string          `db:"map_id"`
+	UserId string          `db:"user_id"`
+	Role   contributorRole `db:"role"`
+}
+
+func NewMapContibutors(mapId string, userId string, role contributorRole) (*MapContributors, error) {
+	if mapId == "" || userId == "" || role == "" {
+		return nil, errors.New("empty map contributors entity fields")
+	}
+	return &MapContributors{
+		MapId:  mapId,
+		UserId: userId,
+		Role:   role,
+	}, nil
 }
--- backend/internal/core/user.go
+++ backend/internal/core/user.go
@@ -1,10 +1,25 @@
 package core
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type User struct {
 	Id           string    `db:"id"`
 	Username     string    `db:"username"`
 	PasswordHash string    `db:"password_hash"`
 	CreatedAt    time.Time `db:"created_at"`
+}
+
+func NewUser(id string, username string, passwordHash string, createdAt time.Time) (*User, error) {
+	if username == "" || passwordHash == "" || createdAt.IsZero() {
+		return nil, errors.New("empty user entity fields")
+	}
+	return &User{
+		Id:           id,
+		Username:     username,
+		PasswordHash: passwordHash,
+		CreatedAt:    createdAt,
+	}, nil
 }
